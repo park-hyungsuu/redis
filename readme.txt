894a8feb87d39a28f6d1dc1c0c88fe2199275280
park-hyungsuu <ix6408@gmail.com>
2026-01-08 21:01:53 +0900

    third

--- readme.txt
+++ readme.txt
@@ -7,6 +7,3 @@ redis 다운로드
 https://github.com/microsoftarchive/redis/releases/
 
 
-추가 개선 사항
-redis pool 사용.
-mysql,redis 동시에 Transaction 적용
--- src/main/java/com/hyungsuu/common/config/RedisConfig.java
+++ src/main/java/com/hyungsuu/common/config/RedisConfig.java
@@ -1,14 +1,22 @@
 package com.hyungsuu.common.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import com.hyungsuu.apigate.samaple.web.UserController;
 
+import io.lettuce.core.api.StatefulConnection;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @PropertySource({ "classpath:/application.properties", "classpath:/ecid/egovProps/globals.properties" })
 public class RedisConfig {
@@ -18,11 +26,51 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.port}")
     private int port;
-
+   
+ //   @Value("${redis.password}")
+//    private String password;
+//    @Value("${redis.clientName}")
+//    private String clientName;
+    
+    @Value("${spring.data.redis.lettuce.pool.max-active}")
+    private int maxTotal;
+    @Value("${spring.data.redis.lettuce.pool.max-idle}")
+    private int maxIdle;
+    @Value("${spring.data.redis.lettuce.pool.min-idle}")
+    private int minIdle;
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        log.info("host: {}", host);
+        log.info("port: {}", port);
+
+        // Redis Config
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+//        redisConfig.setPassword(password);
+
+        // Client Config
+        // Connection Pool을 사용하지 않을 경우 이 설정 사용
+//        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+//                .clientName(clientName)
+//                .build();
+        // Connection Pool을 사용할 경우 이 설정 사용
+        GenericObjectPoolConfig<StatefulConnection<?, ?>> genericObjectPoolConfig = new GenericObjectPoolConfig<StatefulConnection<?, ?>>();
+        genericObjectPoolConfig.setMaxTotal(10000);
+        genericObjectPoolConfig.setMaxIdle(10000);
+        genericObjectPoolConfig.setMinIdle(10);
+		
+        LettucePoolingClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+//                .clientName(clientName)
+                .poolConfig(genericObjectPoolConfig)
+                .build();
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisConfig, clientConfig);
+        connectionFactory.setShareNativeConnection(true);
+        return connectionFactory;
     }
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        return new LettuceConnectionFactory(host, port);
+//    }
 
     //RedisTemplate 사용을 위한 추가
     @Bean
